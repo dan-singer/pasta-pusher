02bec7f4cf5dcda6c730d1e5901986ac38471a56
Dan Singer <dsinger.sax@gmail.com>
2018-11-24 14:17:42 -0500

    Implement GameManager and Item classes

---
+++ Assets/Scenes/Scripts/GameManager.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Main manager class for the game. Uses the Singleton design pattern.
+/// </summary>
+public class GameManager : MonoBehaviour {
+
+	//Singleton pattern
+	private static GameManager instance;
+	public static GameManager Instance
+	{
+		get
+		{
+			if (!instance){
+				instance = GameObject.FindObjectOfType<GameManager>();
+			}
+			return instance;
+		}
+	}
+
+	public event Action<int> PastaChanged;
+
+	private int pasta;
+
+	/// <summary>
+	/// The current amount of pasta
+	/// </summary>
+	public int Pasta{
+		get{
+			return pasta;
+		}
+		private set{
+			pasta = value;
+			if (PastaChanged != null){
+				PastaChanged(pasta);
+			}
+		}
+	}
+
+
+	public event Action<int> PPCChanged;
+	private int ppc;
+
+	/// <summary>
+	/// Pasta-per-click
+	/// </summary>
+	public int PPC{ 
+		get{
+			return ppc;
+		}
+		private set{
+			ppc = value;
+			if (PPCChanged != null){
+				PPCChanged(ppc);
+			}
+		} 
+	}
+
+	public Item[] ShopItems;
+
+	public event Action<Sprite> PastaSpriteChanged;
+
+	public void ReceivePasta()
+	{
+		Pasta += PPC;
+	}
+
+	public void ReceiveUpgrade(Item item)
+	{
+		PPC += item.Value;
+		if (item is PastaItem){
+			PastaItem pi = (PastaItem)item;
+			if (PastaSpriteChanged != null){
+				PastaSpriteChanged(pi.pastaSpr);
+			}
+		}
+	}
+}
---
+++ Assets/Scenes/Scripts/Item.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Item", menuName = "Inventory/Item")]
+public class Item : ScriptableObject {
+	public string Name;
+	public string Description;
+	public int Cost;
+	public int Value;
+}
+
+[CreateAssetMenu(fileName = "PastaItem", menuName = "Inventory/PastaItem")]
+public class PastaItem : Item {
+	public Sprite pastaSpr;
+}
+
